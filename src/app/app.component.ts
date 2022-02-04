3aa7439fd1c4c5a72ba02476d21bbb6d9be697ea
Abigaly <abigaly33@gmail.com>
2022-02-03 23:04:22 -0600

    Descargamos db.jason,  hicimos carpetado, usamos paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Mascota } from './models/mascota.interface';
+import { Mascota } from '@models/mascota.interface';
 
 @Component({
   selector: 'app-root',
@@ -10,45 +10,45 @@ export class AppComponent {
 
 
 
-  public mascota: Mascota;
+  // public mascota: Mascota;
 
-  constructor() {
-    this.opciones();
-    this.menu();
-    this.mascota = {
-      nombre: '',
-      tipo: '',
-      vacunado: false,
-      raza: '',
-      fechaNacimiento: new Date,
-      edad: '1 dia'
-    }
+  // constructor() {
+  //   this.opciones();
+  //   this.menu();
+  //   this.mascota = {
+  //     nombre: '',
+  //     tipo: '',
+  //     vacunado: false,
+  //     raza: '',
+  //     fechaNacimiento: new Date,
+  //     edad: '1 dia'
+  //   }
 
-  }
-  private menu(): void {
-    console.log('*********************MENU**************');
-    console.log('******************Selecciona una opción*********');
-    console.log(' 1 - Crear una mascota');
-  }
+  // }
+  // private menu(): void {
+  //   console.log('*********************MENU**************');
+  //   console.log('******************Selecciona una opción*********');
+  //   console.log(' 1 - Crear una mascota');
+  // }
 
-  private opciones(): void {
-    let toNumber = parseInt(prompt('Elija una opción') as string);
+  // private opciones(): void {
+  //   let toNumber = parseInt(prompt('Elija una opción') as string);
 
-    if (!isNaN(toNumber)) {
-      this.menuOpciones(toNumber);
-    } else {
-      alert('La opcion que ingreso es incorrecta');
-    }
-  }
+  //   if (!isNaN(toNumber)) {
+  //     this.menuOpciones(toNumber);
+  //   } else {
+  //     alert('La opcion que ingreso es incorrecta');
+  //   }
+  // }
 
-  private menuOpciones(opt: number): void {
-    switch(opt){
-      case 1:
+  // private menuOpciones(opt: number): void {
+  //   switch(opt){
+  //     case 1:
 
-      break;
-      default:
-        alert('La opción que ingresó no está disponible');
-    }
-  }
+  //     break;
+  //     default:
+  //       alert('La opción que ingresó no está disponible');
+  //   }
+  // }
 }
 
