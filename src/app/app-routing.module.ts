98cbc3040f56473e07d1373c1ea48edd8b174892
Abigaly <abigaly33@gmail.com>
2022-02-07 13:41:43 -0600

    agregamos register en la carpeta auth, modificamos ruta de login a register, y visevebsa si no hay nada que mostrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule, Component } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '@auth/login/login.component';
-
+import { RegisterComponent } from '@auth/register/register.component';
 
 
 const routes: Routes = [
 
- { path: '', component:LoginComponent }
+
+ { path: '', redirectTo: 'login', pathMatch: 'full'},
+ { path: 'login', component:LoginComponent },
+ { path: 'register', component:RegisterComponent },
+ { path: '**', redirectTo: 'login' }
 
 ];
 
@@ -16,7 +20,6 @@ const routes: Routes = [
 @NgModule({
 
   imports: [RouterModule.forRoot(routes)],
-
   exports: [RouterModule]
 
 })
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import {SharedModule } from '@shared/shared.module';
+import { RegisterComponent } from './register/register.component';
 
 
 
 
 @NgModule({
   declarations: [ //solo portar componentes
-    LoginComponent
+    LoginComponent, RegisterComponent
   ],
   imports: [  //solo se pueden importar modulos
     CommonModule
